0f964fc89b4c6654ad95890e180b2db7fbed8320
NewMBW <115166381+Fatou-hub@users.noreply.github.com>
2023-03-01 13:08:45 +0100

    projet_5

---
+++ content.js
@@ -0,0 +1,25 @@
+let BtnYellowPk = document.getElementById('btnpokemon');
+
+BtnYellowPk.addEventListener('click', setButtonPikachu );
+
+function setButtonPikachu(){
+    console.log("Hello");
+    let images = document.getElementsByTagName('img');//
+    for(let i = 0; i < images.length;i++){
+        chrome.runtime.sendMessage({msg: 'image', index:i},function({data,index}){
+            images[index].src = data.link;
+        });
+    };
+    
+    //========================================================
+    let paragraphs = document.getElementsByTagName('p');
+    let i = 0;
+    
+    while(i < paragraphs.length){
+        paragraphs[i].style['background-color'] = '#ff1493';
+        i++;
+    };
+}
+
+
+
